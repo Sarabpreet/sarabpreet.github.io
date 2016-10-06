e2b3ac715158a9cf3ef626a7a404749f916309ac
Sarabpreet S <Sarabpreetsingh23@gmail.com>
2016-10-06 14:16:44 +0530

    changes++

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+ $(function(){
+      $(".typed").typed({
+        strings: ["Designer", "Developer","Geek","Product Guy @ SignDigital.in"],
+ 
+ 	      startDelay: 3000,
+        typeSpeed: 0,
+        loop:true,
+        backDelay: 500,
+         cursorChar: "|",
+           loopCount: false
+      });
+  });
+
+
+
+  $(function(){
+      $(".work").typed({
+        strings: ["designer ", "developer ", "geek "],
+        startDelay: 3000,
+        typeSpeed: 0,
+        loop:true,
+        backDelay: 500,
+
+      });
+
+
+
+
+  });
+
+
