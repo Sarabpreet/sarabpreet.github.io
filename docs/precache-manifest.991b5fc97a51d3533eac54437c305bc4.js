b45f8849e8fb0d61aa11f2b2f7e4ef739ae15962
Sarabpreet Singh <sarabpreetsingh23@gmail.com>
2020-04-29 01:42:18 +0530

    added basics

---
+++ docs/precache-manifest.991b5fc97a51d3533eac54437c305bc4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0596ebe76d330184a0e1",
+    "url": "/js/about.c2690993.js"
+  },
+  {
+    "revision": "ab5690c614b24170cd69",
+    "url": "/css/app.80434423.css"
+  },
+  {
+    "revision": "ab5690c614b24170cd69",
+    "url": "/js/app.483f1901.js"
+  },
+  {
+    "revision": "f992ad0f4065ec986a1c",
+    "url": "/css/chunk-vendors.96d1351d.css"
+  },
+  {
+    "revision": "f992ad0f4065ec986a1c",
+    "url": "/js/chunk-vendors.57bd8b24.js"
+  },
+  {
+    "revision": "60b1e73c7e42444a6341d9a5ca5921de",
+    "url": "/img/sarabpreet-headshot.60b1e73c.png"
+  },
+  {
+    "revision": "c032c7c9ecb50c197313bec170298caf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
